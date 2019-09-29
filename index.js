7151a37d5ad47778c2fa77da65c696e5a1ea6193
Florian Stellbrink <flo@stellbr.ink>
2019-09-29 23:18:20 +0200

    Use windows binary when executed on win32

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const { spawn } = require('child_process')
 const { EventEmitter } = require('events')
 const onExit = require('signal-exit')
 
-const bin = path.join(__dirname, 'dark-mode-listener')
+const { platform } = process
+
+let bin
+
+if (platform === 'macos') {
+  bin = path.join(__dirname, 'dark-mode-listener')
+} else if (platform === 'win32') {
+  bin = path.join(__dirname, 'dark-mode-listener-windows.exe')
+} else {
+  throw new Error(`Unsupported platform: ${platform}`)
+}
+
 const child = spawn(bin, [], { stdio: ['ignore', 'pipe', 'ignore'] })
 
 onExit(() => child.kill())
